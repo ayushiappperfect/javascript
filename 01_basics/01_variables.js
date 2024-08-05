e0d7de96391b49615761a9d7f7602d4a9ccf1509
ayushi8696 <122522577+ayushi8696@users.noreply.github.com>
2024-08-05 10:50:36 +0000

    variables check 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId= 144554
+let accountEmail="ayushi@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+console.table([accountId,accountEmail,accountCity,accountPassword])
