3b06784f2c29ab5993fde6ae45f2d9780ad1876c
ayushi8696 <122522577+ayushi8696@users.noreply.github.com>
2024-08-05 12:16:07 +0000

    conversionsOperations part 2

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,28 +1,50 @@
-let score="33abc"
+// //conversion to number
+// let score="33abc"
 
-//console.log(typeof score)
-//console.log(typeof(score))
+// //console.log(typeof score)
+// //console.log(typeof(score))
 
-//conversion to number
-let valueInNumber=Number(score)
-//console.log(valueInNumber)
-//console.log(typeof valueInNumber)
+// let valueInNumber=Number(score)
+// //console.log(valueInNumber)
+// //console.log(typeof valueInNumber)
 
-// "33" => 33
-//"33abc" => NaN
-//true =>1 , false=>0
+// // "33" => 33
+// //"33abc" => NaN
+// //true =>1 , false=>0
 
-//conversion to boolean
-let isLoggedIn="ayushi"
-let booleanIsLoggedIn= Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn)
+// //conversion to boolean
+// let isLoggedIn="ayushi"
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// //console.log(booleanIsLoggedIn)
 
-//1=>true ;0=>false
-//""=> false
-//"ayushi"=>true
+// //1=>true ;0=>false
+// //""=> false
+// //"ayushi"=>true
 
-//conversion to String
-let somenumber=33;
-let stringNumber=String(somenumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// //conversion to String
+// let somenumber=33;
+// let stringNumber=String(somenumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// *****************Operations**********//
+let value=3
+let negValue=-value;
+//console.log(negValue)
+
+//console.log(2**3) //power symbol- ** 
+
+//treat first number data Type as datatype for all and then compute the things
+// console.log("1"+2) //12
+// console.log(1+2+'2')//32
+// console.log("1"+2+2)//122
+
+//console.log(+true) //1
+//console.log(true+) //error
+//console.log(+"") //0
+
+let gameCounter=100
+console.log(gameCounter++);
+console.log(++gameCounter)
