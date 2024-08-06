76c89e2e97525a2ca603e28a82af8b0f2b52dd19
ayushi8696 <122522577+ayushi8696@users.noreply.github.com>
2024-08-06 04:36:12 +0000

    dataTypes_summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,17 @@
+/*
+    PRIMITIVE-(7 TYPES)
+      STRING,NUMBER,BOOLEAN,NULL,UNDEFINED,SYMBOL,BIGINT
+    REFERENCE(NON PRIMITIVE)
+      ARRAY,OBJECTS,FUNCTIONS
+    ARRAY-[]
+    OBJECT-{}
+    FUNCTION- function(){}
+    
+    return type of dataTypes-
+      bigint- undefined
+      function- function (object funvtion)
+      null , object- object
+       array- function object
+      symbol- symbol
+      undefined-undefined
+*/
