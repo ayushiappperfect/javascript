d9291bce8ea009e972ce27ace9b0ea28121dcdd5
ayushi8696 <122522577+ayushi8696@users.noreply.github.com>
2024-08-06 12:11:02 +0000

    objects part 2 and json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -30,11 +30,39 @@ const obj3=Object.assign({},obj1,obj2)
 // if you are not giving it then
 // it will take first object as target and rest as source
 const obj4={...obj1,...obj2}
-console.log(obj3)   
-console.log(obj4)
+// console.log(obj3)   
+// console.log(obj4)
 
-console.log(Object.keys(instaUser))
-console.log(Object.values(instaUser))
-console.log(Object.entries(instaUser))
+// console.log(Object.keys(instaUser))
+// console.log(Object.values(instaUser))
+// console.log(Object.entries(instaUser))
 
-console.log(instaUser.hasOwnProperty('isLogged'))
+// console.log(instaUser.hasOwnProperty('isLogged'))
+
+const course={
+    course: "javascript",
+    price:"1",
+    courseInstructor:"ayushi"
+}
+
+
+
+////Destructing Objects//////
+// course.courseInstructor
+const {courseInstructor: Instructor}=course   //const {value want to be extracted}=from where value is extracted
+                         //renaming courseInstructor as Instructor
+console.log(Instructor)
+
+///JSON FORMAT///
+/* 
+  {
+   "name":"ayushi",
+   "email":"a@gmail.com"
+  }
+
+  [
+    {},
+    {},
+    {}
+  ]
+*/
